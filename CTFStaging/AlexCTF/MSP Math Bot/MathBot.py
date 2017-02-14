a1cd1ab1f7a1cfbdab24248f6aeaf7e5569abdc1
Michael <codingo@outlook.com>
2017-02-15 09:15:52 +1000

    FEATURE: AlexCTF MathBot

---
+++ CTFStaging/AlexCTF/MSP Math Bot/MathBot.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+ 
+import socket
+import re
+ 
+if __name__ == '__main__':
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('195.154.53.62', 1337))
+ 
+    while True:
+        data = b''
+        while True:
+            chunk = client.recv(4096)
+            data += chunk
+            if len(chunk) < 4096:
+                break
+ 
+        # our flag contains ALEXCTF, once it's revealed print recevied data and exit
+        if 'ALEXCTF' in data.decode('utf-8'):
+            print(data.decode('utf-8'))
+            break
+        
+        # \d+ matches a digit (equal to [0-9])
+        # .{3} matches any  character, except line terminators exactly three times
+        m = re.search('\d+.{3}\d+', data.decode('utf-8'))
+        expression = m.group(0)
+ 
+        #properly handle division
+        if '/' in expression:
+            expression = expression.replace('/', '//')
+ 
+        result = eval(expression)
+ 
+        #print results to screen to see script progress
+        print(expression + ' = ' + str(result))
+ 
+        #encode and transfer
+        data = str(result).encode('utf-8') + b'\n'
+        client.send(data)
