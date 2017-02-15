3ad07b057db077f0f8b42178be9bcc31e4a3baea
Michael <codingo@outlook.com>
2017-02-15 13:40:37 +1000

    Updated code comments to py standard

--- CTFStaging/AlexCTF/MSP Math Bot/MathBot.py
+++ CTFStaging/AlexCTF/MSP Math Bot/MathBot.py
@@ -21,13 +21,15 @@ if __name__ == '__main__':
  
     while True:
         data = b''
+
+        # receive and store data
         while True:
             chunk = client.recv(MAXBUF)
             data += chunk
             if len(chunk) < MAXBUF:
                 break
         
-        #store decoded data for future usage
+        # store decoded data for future usage
         decoded = data.decode('utf-8')
 
         # our flag contains ALEXCTF, once it's revealed print recevied data and exit
@@ -43,15 +45,15 @@ if __name__ == '__main__':
         
         expression = match.group(0)
  
-        #properly handle division
+        # properly handle division
         if '/' in expression:
             expression = expression.replace('/', '//')
  
         result = eval(expression)
  
-        #print results to screen to see script progress
+        # print results to screen to see script progress
         print(expression + ' = ' + str(result))
 
-        #encode and transfer
+        # encode and transfer
         data = str(result).encode('utf-8') + b'\n'
         client.send(data)
