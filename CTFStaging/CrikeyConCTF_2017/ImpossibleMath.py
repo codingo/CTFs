1cce478c0ed5845f893fa9e109c86b0f4200d39f
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-03-05 22:38:38 +1000

    Updated for stating solution to impossible math

---
+++ CTFStaging/CrikeyConCTF_2017/ImpossibleMath.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+ 
+import socket
+import re
+import operator
+import sys
+
+MAXBUF = 4096
+SENTINEL = 'flag'
+CTF_BOT = ('crikeyconctf.dook.biz', 43981)
+
+def int_overflow(val):
+  if not -sys.maxsize -1 <= val <= sys.maxsize:
+    val = (val + (sys.maxsize + 1)) % (2 * (sys.maxsize + 1)) - sys.maxsize - 1
+  return val
+
+if __name__ == '__main__':
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(CTF_BOT)
+ 
+    x = 0
+
+    while True:
+        data = b''
+
+        # receive and store data
+        while True:
+            chunk = client.recv(MAXBUF)
+            data += chunk
+            if len(chunk) < MAXBUF:
+                break
+        
+        # store decoded data for future usage
+        decoded = data.decode('utf-8')
+        
+        # print out response packet
+        print(decoded)
+
+        # our flag contains flag{}, once it's revealed print recevied data and exit
+        if SENTINEL in decoded:
+            print(decoded)
+            break
+
+        # skip loop until we see calculation
+        if not re.search('[>*]', decoded):
+            continue
+
+        # select number and store
+        x = re.search('\d+', decoded)
+
+        
+        expression = match.group(0)
+ 
+        # properly handle division
+        if '/' in expression:
+            expression = expression.replace('/', '//')
+ 
+        result = eval(expression)
+ 
+        # print results to screen to see script progress
+        print(expression + ' = ' + str(result))
+
+        # encode and transfer
+        data = str(result).encode('utf-8') + b'\n'
+        print('Sending: ' + str(result))
+        client.send(data)
